9a2af482f1a2c017e8d3257de4bc4bd67d05a4e5
Nick Wells <wells.nicholas@gmail.com>
2023-12-06 22:04:52 +0000

    feature(colfmt): add the Bool formatter

---
+++ col/colfmt/boolFormatter.go
@@ -0,0 +1,36 @@
+package colfmt
+
+import (
+	"fmt"
+
+	"github.com/nickwells/col.mod/v3/col"
+)
+
+// Bool records the values needed for the formatting of a
+// bool value.
+type Bool struct {
+	// W gives the minimum width of the bool that should be printed
+	W int
+	// StrJust gives the justification to be used
+	StrJust col.Justification
+	// IgnoreNil, if set to true will make nil values print as the empty bool
+	IgnoreNil bool
+}
+
+// Formatted returns the value formatted as a bool
+func (f Bool) Formatted(v any) string {
+	if f.IgnoreNil && v == nil {
+		return ""
+	}
+	return fmt.Sprintf("%t", v)
+}
+
+// Width returns the intended width of the value
+func (f Bool) Width() int {
+	return f.W
+}
+
+// Just returns the justification of the value
+func (f Bool) Just() col.Justification {
+	return f.StrJust
+}
